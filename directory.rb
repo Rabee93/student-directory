3b38dabfdfad1281e71163ce794e46ca3c4b7547
EC2 Default User <ec2-user@ip-172-31-28-29.eu-west-2.compute.internal>
2020-06-01 12:14:56 +0000

    iterated over students array and used count method on it

--- directory.rb
+++ directory.rb
@@ -1,20 +1,10 @@
-student_count = 11
+
 # lets put all students in an array.
 students = [ "Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the west", "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon", "Norman Bates"]
 # and print them.
 puts "The students of villains academy"
 puts "--------------"
 
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+students.each do |s| puts s end 
 # finally we print the total
-puts " Overall we have #{student_count} great students"
+puts " Overall we have #{student.count} great students"
