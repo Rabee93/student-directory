31c056c5c0c8ad8d41c692132adec80c481e0fbe
EC2 Default User <ec2-user@ip-172-31-28-29.eu-west-2.compute.internal>
2020-06-01 14:25:59 +0000

    added hashes and modified methods

--- directory.rb
+++ directory.rb
@@ -1,18 +1,18 @@
 
 # lets put all students in an array.
-students = [ "Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the west", "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon", "Norman Bates"]
+students = [{name: "Dr. Hannibal Lecter", cohort: :november}, {name: "Darth Vader", cohort: :november}, {name: "Nurse Ratched", cohort: :november}, {name: "Michael Corleone", cohort: :november}, {name: "Alex DeLarge", cohort: :november}, {name: "The Wicked Witch of the west", cohort: :november}, {name: "Terminator", cohort: :november}, {name: "Freddy Krueger", cohort: :november}, {name: "The Joker", cohort: :november}, {name: "Joffrey Baratheon", cohort: :november}, {name: "Norman Bates", cohort: :november}]
 
 
 def print_header
-puts "The students of villains academy"
-puts "--------------"
+  puts "The students of villains academy"
+  puts "--------------"
 end 
-def print(names)
-names.each do |s| puts s end 
+def print(students)
+  students.each do |s| puts "#{s[:name]} (#{s[:cohort]} cohort)" end 
 end 
 # finally we print the total
 def print_footer(names)
-puts " Overall we have #{names.count} great students"
+  puts " Overall we have #{names.count} great students"
 end 
 # nothing happens until we call a method.
 print_header 
