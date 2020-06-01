23a04ea67e4b3dfa11ed43ebd80d0860ccbb3077
EC2 Default User <ec2-user@ip-172-31-28-29.eu-west-2.compute.internal>
2020-06-01 12:41:33 +0000

    we added methods to me it easier to edit

--- directory.rb
+++ directory.rb
@@ -1,10 +1,20 @@
 
 # lets put all students in an array.
 students = [ "Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the west", "Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon", "Norman Bates"]
-# and print them.
+
+
+def print_header
 puts "The students of villains academy"
 puts "--------------"
-
-students.each do |s| puts s end 
+end 
+def print(names)
+names.each do |s| puts s end 
+end 
 # finally we print the total
-puts " Overall we have #{student.count} great students"
+def print_footer(names)
+puts " Overall we have #{names.count} great students"
+end 
+# nothing happens until we call a method.
+print_header 
+print(students)
+print_footer(students)
