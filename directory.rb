423516110d849c433f59af876a3790a4824ad08c
EC2 Default User <ec2-user@ip-172-31-28-29.eu-west-2.compute.internal>
2020-06-04 11:17:18 +0000

    interactive student program

--- directory.rb
+++ directory.rb
@@ -1,17 +1,28 @@
-def input_students
-  puts "please enter the names of the students"
+
+ months = ["january","february","march", "april","may", "june", "july", "august", "september", "october", "november", "december"]
+
+
+def input_students(months)
+  puts "please enter the names of the students and then the cohort"
   puts "to finish just hit, return twice"
-# create an empty array
+
  students = []
- # get the first name
- name = gets.chomp
- # while the name is not empty, repeat this code
- while !name.empty? do
-   # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "now we have #{students.count} students"
-    name = gets.chomp 
- end 
+ name = gets[0...-1]
+ cohort = gets[0...-1]
+  
+  while !name.empty? do
+  if cohort.empty?
+    cohort = "november"
+  end 
+  until months.include? cohort
+    puts "try again"
+    cohort = gets[0...-1]
+  end 
+    students << {name: name, cohort: cohort.to_sym, hobbies: :tennis, country_of_birth: :UK}
+    print_footer(students) 
+    name = gets[0...-1]
+    cohort = gets[0...-1]
+  end 
   students
 end 
 
@@ -19,15 +30,37 @@ def print_header
   puts "The students of villains academy"
   puts "--------------"
 end 
-def print(students)
-  students.each do |s| puts "#{s[:name]} (#{s[:cohort]} cohort)" end 
+def print(students, letter)
+  index = 0
+  while index < students.length
+    s = students[index] 
+    if s[:name].start_with?(letter) && s[:name].length < 12
+      puts "#{index + 1}.#{s[:name]} (#{s[:cohort]} cohort)".center(30)
+    end
+    index +=1
+  end 
 end 
-# finally we print the total
+
 def print_footer(names)
-  puts " Overall we have #{names.count} great students"
+  students_count = names.count 
+  if students_count == 1
+    puts "Overall we have #{students_count} great student"
+  else
+  puts "Overall we have #{students_count} great students"
+  end 
 end 
-# nothing happens until we call a method.
-students = input_students 
-print_header 
-print(students)
+
+students = input_students(months)
+if students.length > 0
+  print_header 
+  group_students = []
+  months.each do |m| 
+  students.each do |s| 
+    if s [:cohort] == m.to_sym
+      group_students << s
+    end 
+  end 
+end 
+print(group_students, "s") 
 print_footer(students)
+end 
